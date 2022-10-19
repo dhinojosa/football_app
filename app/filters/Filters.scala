aa8831d438d6e0565128d6ce6102acfd84cbfcf2
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-10-19 11:44:46 -0600

    Wrong filter was imported

--- app/filters/Filters.scala
+++ app/filters/Filters.scala
@@ -1,12 +1,10 @@
 package filters
 
-import akka.event.LoggingFilter
-
 import javax.inject.Inject
 import play.api.http.DefaultHttpFilters
 import play.filters.gzip.GzipFilter
 
 class Filters @Inject() (
-                            gzip: GzipFilter,
-                            log: LoggingFilter
-                        ) extends DefaultHttpFilters(gzip, log)
+    gzip: GzipFilter,
+    log: LoggingFilter
+) extends DefaultHttpFilters(gzip, log)
